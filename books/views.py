017a1e60f652729f36e99d85ae63f89704fa8593
Saman Salimzade <113477417+samansmd94@users.noreply.github.com>
2025-08-19 21:52:05 -0700

    add bookCreateVire

--- books/views.py
+++ books/views.py
@@ -13,3 +13,7 @@ class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
 
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_create.html'
