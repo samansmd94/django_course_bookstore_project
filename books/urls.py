947458f8db6edc11f3fcf0f967cfb4013ec76436
Saman Salimzade <113477417+samansmd94@users.noreply.github.com>
2025-08-20 23:30:02 -0700

    add Update and Delete View

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,7 @@ from .views import (
     BookListView,
     BookDetailView,
     BookCreateView,
-    BookUpdateView)
+    BookUpdateView, BookDeleteView)
 
 
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
     path('create/', BookCreateView.as_view(), name='book_create'),
     path('<int:pk>/edit/', BookUpdateView.as_view(), name='book_update'),
+    path('<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
 ]
